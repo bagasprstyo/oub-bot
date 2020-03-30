5e9aed422707f2c3e991970836d9a839afd94d8f
bagasprstyo <62801235+bagasprstyo@users.noreply.github.com>
2020-03-30 17:54:13 +0700

    Update pmpermit.py

--- userbot/modules/pmpermit.py
+++ userbot/modules/pmpermit.py
@@ -17,10 +17,12 @@ from userbot.events import register
 
 # ========================= CONSTANTS ============================
 UNAPPROVED_MSG = (
-    "`HeY! This is an automated message.\n\n`"
-    "`I haven't approved you to PM yet.`"
-    "`Please wait for me to look in, I mostly approve PMs.\n\n`"
-    "`Until then, please don't spam my Mastor's PM, you'll get blocked and reported if you do so!`")
+    "`❌ANDA MEMASUKI KAWASAN IntanArap❌.\n\n`"
+    "`•Tunggu Sampai @IntanArap Melihat Ini!..`"
+    "`🚫NOSPAM IN HERE! SPAM? I AM BLOCK YOU!!...\n\n`"
+    "`Terimakasih Semoga Anda mengerti`")
+    "` *This is an automated message.`")
+    "` ❤️IntanArap❤️
 # =================================================================
 
 
